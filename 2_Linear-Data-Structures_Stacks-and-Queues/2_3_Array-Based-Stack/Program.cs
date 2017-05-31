b99efdbf9162b5c54c473dcf827e957e7da179d4
llalov <luchoazz@abv.bg>
2017-05-31 10:08:04 +0300

    added stacks and queues exercises

---
+++ 2_Linear-Data-Structures_Stacks-and-Queues/2_3_Array-Based-Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _2_3_Array_Based_Stack
+{
+    class Program
+    {
+        static void Main()
+        {
+            var test = new ArrayStack<int>();
+
+            test.Push(12);
+            test.Push(20);
+            test.Push(33);
+            test.Push(23);
+            Console.WriteLine(test.Count);
+            Console.WriteLine(test.Pop());
+            Console.WriteLine(test.Count);
+
+            int[] output = test.ToArray();
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+    }
+}
