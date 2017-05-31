e86d36287d70b063c2a144196d609f71aeaec3f8
llalov <luchoazz@abv.bg>
2017-05-31 09:43:36 +0300

    updated lists exercises

--- 1_Liear-Data-Structures_Lists/1_1_Sum-and-Average/Program.cs
+++ 1_Liear-Data-Structures_Lists/1_1_Sum-and-Average/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _1_1_Sum_and_Average
 {
@@ -10,6 +8,23 @@ namespace _1_1_Sum_and_Average
     {
         static void Main(string[] args)
         {
+            try
+            {
+                List<int> input = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x)).ToList();
+
+                decimal sum = 0m;
+                decimal average = 0m;
+                for (int i = 0; i < input.Count; i++)
+                {
+                    sum += input[i];
+                }
+                average = sum / input.Count;
+                Console.WriteLine("Sum={0};\nAverage={1};", sum, average);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Sum=0;\nAverage=0;");
+            }
         }
     }
 }
