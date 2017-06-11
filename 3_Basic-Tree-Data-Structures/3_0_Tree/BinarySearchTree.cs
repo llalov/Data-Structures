0cfb563d540bebdc5bb63b1678564695ccc6f614
llalov <luchoazz@abv.bg>
2017-06-11 23:06:40 +0300

    added BST 'in order' traverse and 'contains' methods

--- 3_Basic-Tree-Data-Structures/3_0_Tree/BinarySearchTree.cs
+++ 3_Basic-Tree-Data-Structures/3_0_Tree/BinarySearchTree.cs
@@ -8,6 +8,57 @@ namespace _3_0_Tree
 {
     public class BinarySearchTree<T> where T : IComparable<T>
     {
+        private Node root;
+
+        private class Node
+        {
+            public Node(T value)
+            {
+                this.Value = value;
+            }
+
+            public T Value { get; set; }
+            public Node Left { get; set; }
+            public Node Right { get;  set;}
+
+        }
+
+        private void EachInOrder(Node node, Action<T> action)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            this.EachInOrder(node.Left, action);
+            action(node.Value);
+            this.EachInOrder(node.Right, action);
+        }
+
+        private Node FindElement (T element)
+        {
+            Node currentNode = this.root;
+
+            while (currentNode != null)
+            {
+                if (element.CompareTo(currentNode.Value) > 0)
+                {
+                    currentNode = currentNode.Right;
+                }
+
+                else if (element.CompareTo(currentNode.Value) < 0)
+                {
+                    currentNode = currentNode.Left;
+                }
+
+                else
+                {
+                    break;
+                }
+            }
+
+            return currentNode;
+        }
 
         public void Insert(T value)
         {
@@ -16,7 +67,9 @@ namespace _3_0_Tree
 
         public bool Contains(T value)
         {
-            throw new NotImplementedException();
+            Node current = this.FindElement(value);
+
+            return current != null;
         }
 
         public void DeleteMin()
@@ -36,7 +89,7 @@ namespace _3_0_Tree
 
         public void EachInOrder(Action<T> action)
         {
-            throw new NotImplementedException();
+            this.EachInOrder(this.root, action);
         }
     }
 
