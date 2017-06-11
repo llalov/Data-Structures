9a5b4b1dd98cd756ffe9b3bcb0151d890791a519
llalov <luchoazz@abv.bg>
2017-06-11 15:39:44 +0300

    added binary search tree class and BST unit tests

---
+++ 3_Basic-Tree-Data-Structures/3_0_Tree/BinarySearchTree.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_0_Tree
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    {
+
+        public void Insert(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteMin()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BinarySearchTree<T> Search(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Range(T startRange, T endRange)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EachInOrder(Action<T> action)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
