5fd784233c3e8b227df8a6ea7cf01e746b8a59e7
llalov <luchoazz@abv.bg>
2017-06-19 01:32:31 +0300

    added Insert method

--- 4_Heaps-Priority-Queues/4_0_BinaryHeap/BinaryHeap.cs
+++ 4_Heaps-Priority-Queues/4_0_BinaryHeap/BinaryHeap.cs
@@ -7,27 +7,59 @@ namespace _4_0_BinaryHeap
     {
         private List<T> heap;
 
+        private bool IsLess(T parent, int index)
+        {
+            T child = this.heap[index];
+            bool result = child.CompareTo(parent) > 0;
+
+            return result;
+        }
+
+        private T Parent(int index)
+        {
+            return this.heap[index - 1];
+        }
+
+        private void Swap(int childIndex, T parent)
+        {
+            int parentIndex = this.heap.IndexOf(parent);
+            this.heap[parentIndex] = this.heap[childIndex]; 
+            this.heap[childIndex] = parent;
+        
+        }
+
+        private void HeapifyUp(int index)
+        {
+            while (index > 0 && IsLess(Parent(index), index))
+            {
+                this.Swap(index, Parent(index));
+                int parentIndex = this.heap.IndexOf(Parent(index));
+                index = parentIndex;
+            }
+        }
+
         public BinaryHeap()
         {
-            // TODO
+            this.heap = new List<T>();
         }
 
         public int Count
         {
             get
             {
-                throw new NotImplementedException();
+                return this.heap.Count;
             }
         }
 
         public void Insert(T item)
         {
-            throw new NotImplementedException();
+            this.heap.Add(item);
+            this.HeapifyUp(this.heap.Count - 1);
         }
 
         public T Peek()
         {
-            throw new NotImplementedException();
+            return this.heap[0];
         }
 
         public T Pull()
