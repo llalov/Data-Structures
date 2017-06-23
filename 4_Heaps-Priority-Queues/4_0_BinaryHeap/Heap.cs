4e98060fced11c6613b9bebbf2fd317704bf8afc
llalov <luchoazz@abv.bg>
2017-06-23 10:03:41 +0300

    added heap "Sort" mtd skeleton

--- 4_Heaps-Priority-Queues/4_0_BinaryHeap/Heap.cs
+++ 4_Heaps-Priority-Queues/4_0_BinaryHeap/Heap.cs
@@ -6,7 +6,29 @@ namespace _4_0_BinaryHeap
     {
         public static void Sort(T[] arr)
         {
-            throw new NotImplementedException();
+            int n = arr.Length;
+
+            for (int i = n/2; i >= 0; i--)
+            {
+                //TODO heapify Element at i
+            }
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                //TODO Swap 0 with i
+                //Heapify down element at 0 
+            }
+        }
+
+        private static void Down(T[] arr, int current, int border)
+        {
+            while (current < border / 2)
+            {
+                //TODO: get greater child
+                //TODO: if current is greater than greater child -> break
+                //TODO: swap elements
+                //TODO: update the index
+            }
         }
     }
 }
