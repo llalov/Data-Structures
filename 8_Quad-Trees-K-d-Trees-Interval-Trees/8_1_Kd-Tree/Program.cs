71377fe53d64019777cb36409caec751589a6510
llalov <luchoazz@abv.bg>
2017-07-16 19:58:08 +0300

    added "K-d tree" skeleton

---
+++ 8_Quad-Trees-K-d-Trees-Interval-Trees/8_1_Kd-Tree/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _8_1_Kd_Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KdTree tree = new KdTree();
+            tree.Insert(new Point2D(5, 5));
+            tree.Insert(new Point2D(3, 2));
+            tree.Insert(new Point2D(2, 6));
+            tree.Insert(new Point2D(8, 8));
+            tree.Insert(new Point2D(8, 9));
+        }
+    }
+}
