9469950379ba56388a79ec748c8fb6b8cc0de935
llalov <luchoazz@abv.bg>
2017-08-19 22:16:01 +0300

    added Ordered Set

---
+++ 9_Hash-Tables-Sets-and-Dictionaries/9_2_Ordered-Set/OrderedSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _9_2_Ordered_Set
+{
+    public class OrderedSet<T> : RedBlackTree<T> where T : IComparable
+    {
+        public void Add(T element)
+        {
+            this.Insert(element);
+        }
+        public void Remove(T element)
+        {
+            this.Delete(element);
+        }
+
+        //TODO: UnionWith(set)
+        //TODO: IntersectWiht(set)
+        //TODO: ExceptWith(set)
+        //TODO: SymmetricExceptWith(set)
+    }
+}
---
+++ 9_Hash-Tables-Sets-and-Dictionaries/9_2_Ordered-Set/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_2_Ordered_Set
+{
+    class Program
+    {
+        static void Main()
+        {
+            var set = new OrderedSet<int>();
+            set.Add(17);
+            set.Add(9);
+            set.Add(12);
+            set.Add(12);
+            set.Add(19);
+            set.Add(6);
+            set.Add(25);
+            set.Add(25);
+
+            List<int> nodes = new List<int>();
+            set.EachInOrder(nodes.Add);
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine(node);
+            }
+        }
+    }
+}
